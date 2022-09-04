53e23b76806b6e000ff384c9ec7477cd344b0f1d
Joseph Edwards Rean <joseph.edwardsrean@outlook.com>
2022-09-04 14:11:50 +0100

    node scraper runs

---
+++ scraperTests.js
@@ -0,0 +1,91 @@
+require('dotenv').config();
+var HTMLParser = require('node-html-parser');
+// const fs = require('fs');
+
+class ScraperApi {
+  constructor(){
+    this.eventList= [];
+    this.matchedFoods=[];
+  }
+
+  async ratFoodScraper(searchStr){
+    try {
+    const scraperapiClient = require('scraperapi-sdk')(process.env.KEY)
+    let foodResult = await scraperapiClient.get('http://www.isamurats.co.uk/vegetables-and-fruits.html')
+    searchStr = this.capitaliseSearch(searchStr)
+    foodResult = this.parseHTML(foodResult)
+    this.ratFoodResults(searchStr, foodResult)
+    } catch (err) {
+      console.log(err);
+    }
+   }
+
+  capitaliseSearch(searchStr){
+    return searchStr.charAt(0).toUpperCase() + searchStr.slice(1);
+  }
+
+  ratFoodResults(searchStr, foodResult){
+    const foodSearch = foodResult.querySelectorAll('strong');
+    foodSearch.forEach(food=> {
+      if (food.textContent.includes(searchStr)){
+        let foodName = food.textContent;
+        let foodContent = (food.nextSibling).textContent;
+        this.matchedFoods.push([foodName, foodContent]);
+      }
+    })
+    return this.matchedFoods;
+  }
+
+  
+  async ratEventsScraper(){
+    try{
+      const scraperapiClient = require('scraperapi-sdk')(process.env.KEY)
+      let eventApiResult = await scraperapiClient.get('https://www.nfrs.org/shows/next/')
+      eventApiResult = this.parseHTML(eventApiResult)
+      this.ratEventsResults(eventApiResult)
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  ratEventsResults(eventApiResult){
+    let totalShows = this.showCalculator(eventApiResult)
+    const removeWhitesmpaceRegex = /^(?=\n)$|^\s*|\s*$|\n\n+/gm;
+    eventApiResult = eventApiResult.querySelector('p').nextElementSibling;
+    this.eventList.push(eventApiResult.textContent.replace(removeWhitesmpaceRegex, ""));
+    for (let i = 0; i < 6; i++){
+      console.log(i)
+      eventApiResult = eventApiResult.nextElementSibling;
+      this.eventList.push(eventApiResult.textContent.replace(removeWhitesmpaceRegex, ""));
+    }
+    this.eventList = JSON.parse(this.eventList)
+    return this.eventList
+  }
+
+  showCalculator(data){
+    let showsNum = data.querySelector('h2').textContent
+    const findIntegerRegex = /\d+/g;
+    showsNum = showsNum.match(findIntegerRegex);
+    showsNum = parseInt(showsNum.join(""));
+    return showsNum;
+  }
+
+  parseHTML(data){
+    return HTMLParser.parse(data)
+   }
+}
+
+const scraper = new ScraperApi();
+
+// scraper.ratFoodScraper("Apple").then(()=>{
+//   scraper.matchedFoods.forEach(food=>{
+//     console.log(food.join('\n'))
+//   });
+// })
+
+scraper.ratEventsScraper().then(()=>{
+  scraper.eventList.forEach(event=>console.log(event));
+})
+
+
+module.exports = ScraperApi;

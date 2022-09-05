7e70b0aac3e09998468fde70e7c00f2cc4bd20b2
Joseph Edwards Rean <joseph.edwardsrean@outlook.com>
2022-09-05 16:41:01 +0100

    minor styling

--- client/assets/components/eventComponent.js
+++ client/assets/components/eventComponent.js
@@ -3,7 +3,14 @@ import {View, Text} from 'react-native'
 
 export  function Event({data}){
   return (
-    <View>
+    <View style={{
+      // backgroundColor: '#a9a9a9',
+      width: 320,
+      borderColor: '#dcdcdc',
+      borderWidth: 3,
+      justifyContent: 'center',
+      borderRadius: 5
+    }}>
       <Text>{data.event}</Text>
     </View>
   );
--- client/src/screens/Events.js
+++ client/src/screens/Events.js
@@ -2,12 +2,11 @@ import React, { useState } from 'react'
 import { Text, View, Button, SafeAreaView, ScrollView } from 'react-native'
 import { styles } from '../styles'
 import {Event} from '../../assets/components/eventComponent'
-
 export function Events ({ navigation }) {
 
   const [loading, setLoading]= useState([])
-
   const [data, setData] = useState([])
+  
   React.useEffect(() => {
     fetch('http://localhost:8080/events').then((response) => response.json())
       .then((data) => {
@@ -27,11 +26,6 @@ export function Events ({ navigation }) {
     <ScrollView>
     <View style={styles.container}>
       <Text>Upcoming Events:</Text>
-      {/* {data && (<Text style={{ flex: 1, flexDirection: 'row' }}>{data.map((event)=> {
-        <Text>{event}</Text>
-      })}</Text>)} */}
-      {/* {data && (<Text style={{ flex: 1, flexDirection: 'row' }}>{data[6].event}</Text>)} */}
-
       <ScrollView>
         {loading ? (
           <Text>loading...</Text>
@@ -39,7 +33,6 @@ export function Events ({ navigation }) {
       <></>}
       {renderEvent()}
       </ScrollView>
-
       <Button title="Go to Home" onPress={() => navigation.navigate('Welcome')} />
       <Button title="Go back" onPress={() => navigation.goBack()} />
     </View>

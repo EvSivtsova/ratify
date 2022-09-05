7cacec19c460863f9a546c683fa6955ab1c2fd83
Arsenii Gabov <agabov38@gmail.com>
2022-09-05 13:19:46 +0100

    added map displaying

--- src/screens/Animal.js
+++ src/screens/Animal.js
@@ -1,14 +1,23 @@
-import { Text, View, Button } from 'react-native';
-import { styles } from '../styles'
+import { Text, View, Button } from "react-native";
+import { styles } from "../styles";
 
 export function Animal({ navigation }) {
   return (
     <View style={styles.container}>
       <Text>My profile</Text>
-      <Button title="Go to Food Safety" onPress={() => navigation.navigate('FoodSafety')} />
-      <Button title="Go to Vets" onPress={() => navigation.navigate('Vets')} />
-      <Button title="View events" onPress={() => navigation.navigate('Events')} />
-      <Button title="Go to Home" onPress={() => navigation.navigate('Welcome')} />
+      <Button
+        title="Go to Food Safety"
+        onPress={() => navigation.navigate("FoodSafety")}
+      />
+      <Button title="Go to Vets" onPress={() => navigation.navigate("Vets")} />
+      <Button
+        title="View events"
+        onPress={() => navigation.navigate("Events")}
+      />
+      <Button
+        title="Go to Home"
+        onPress={() => navigation.navigate("Welcome")}
+      />
       <Button title="Go back" onPress={() => navigation.goBack()} />
     </View>
   );

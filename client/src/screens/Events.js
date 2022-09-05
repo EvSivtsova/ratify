4dc7dbd7074796ce20cfb710a42b8e63c9ade40a
Joseph Edwards Rean <joseph.edwardsrean@outlook.com>
2022-09-05 09:54:07 +0100

    backup of event client

--- client/src/screens/Events.js
+++ client/src/screens/Events.js
@@ -3,15 +3,16 @@ import { Text, View, Button } from 'react-native';
 import { styles } from '../styles'
 
 export function Events({ navigation }) {
-  const [data, setData] = useState("")
+  const [data, setData] = useState("LOADING")
   React.useEffect(()=> {
-    fetch('http://localhost:4000/events').then((response)=>
-    setData(response))
+    fetch('http://localhost:8080/events').then((response)=>
+    setData(JSON.stringify(response)))
   },[])
   return (
 
     <View style={styles.container}>
-      <Text>Events{data.forEach(i => console.log(i))}</Text>
+      <Text>hello, here is some data:</Text>
+        <Text>{data}</Text>
       <Button title="Go to Home" onPress={() => navigation.navigate('Welcome')} />
       <Button title="Go back" onPress={() => navigation.goBack()} />
     </View>

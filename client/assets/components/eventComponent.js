d7d7f9eda74e1dfaffe8e02b66c8ed449eddf6b0
Joseph Edwards Rean <joseph.edwardsrean@outlook.com>
2022-09-06 14:07:53 +0100

    added blank line between event elements

--- client/assets/components/eventComponent.js
+++ client/assets/components/eventComponent.js
@@ -3,16 +3,19 @@ import {View, Text} from 'react-native'
 
 export  function Event({data}){
   return (
-    <View style={{
-      // backgroundColor: '#a9a9a9',
-      width: 320,
-      backgroundColor: '#869471',
-      borderColor: '#869471',
-      borderWidth: 3,
-      // justifyContent: 'center',
-      borderRadius: 5
-    }}>
-      <Text>{data.event}</Text>
+    <View>
+      <View style={{
+        // backgroundColor: '#a9a9a9',
+        width: 320,
+        backgroundColor: '#869471',
+        borderColor: '#869471',
+        borderWidth: 3,
+        // justifyContent: 'center',
+        borderRadius: 5
+      }}>
+        <Text>{data.event}</Text>
+      </View>
+    <Text></Text>
     </View>
   );
 }

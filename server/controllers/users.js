866dd9f8327124894bd3fd878d93dae4743057d5
Evgenia Sivtsova <evgenia.sivtsova@gmail.com>
2022-09-07 22:04:38 +0100

    Update server following mergers

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -15,6 +15,44 @@ const UsersController = {
       res.json(user);
       // res.json(User.findOne({ email: user.email }));
     }
+  },
+  Login: (req, res) => {
+    let username = req.body.username;
+    let password = req.body.password;
+  
+    console.log("username: ", username);
+    console.log("password: ", password);
+  
+    let MongoClient = require('mongodb').MongoClient
+    // refactor the server ip
+    MongoClient.connect("mongodb://0.0.0.0/ratify", function(err, client) {
+      if (err) {
+        console.log("mongodb error: ", error);
+      }
+  
+      console.log(client);
+      console.log(client.db);
+      let collection = client.db("ratify").collection('users');
+      collection.findOne({"email": username, "password": password}, function(err, item) {
+        // if OK, user exists, if KO user does not exist
+        console.log(err);
+        console.log(item);
+        if (item == null) {
+          res.send(500);
+        }
+        else {
+          res.send(200);
+        }
+      });
+    });
+  
+    // if ((username == "delly@email.com") && (password == "123456")) {
+    //   res.send("OK");
+    // }
+    // else {
+    //   res.status(500)
+    //   res.send('KO')
+    // }
   }
 };
 

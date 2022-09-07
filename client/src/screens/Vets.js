a9d890a75cbfdde993669bc31f172dd808586bd2
Arsenii Gabov <agabov38@gmail.com>
2022-09-07 16:31:36 +0100

    added styling to vets screen

--- client/src/screens/Vets.js
+++ client/src/screens/Vets.js
@@ -1,6 +1,7 @@
 import { List } from "react-native-paper";
-import { Button, Text } from "react-native";
+import { TouchableOpacity, Text } from "react-native";
 import React, { useState } from "react";
+import { LoginStyle } from "../components/LogInPage/LoginStyle";
 
 export function Vets({ navigation }) {
   const [expanded, setExpanded] = React.useState(true);
@@ -8,45 +9,46 @@ export function Vets({ navigation }) {
   const handlePress = () => setExpanded(!expanded);
 
   return (
-    <List.Section title="Accordions">
+    <List.Section>
+      <Text style={LoginStyle.header}>Pet Services</Text>
       <List.Accordion
+        style={LoginStyle.buttonText}
         title="Vets"
         left={(props) => <List.Icon {...props} icon="folder" />}
       >
-        <Text
-          style="text-indent: 50px;
-  text-align: justify;
-  letter-spacing: 3px;"
-        >
+        <Text style={LoginStyle.LogInButton}>
           Find an exotic pet vet near you that. There are vets that treat a
           large variety of exotics pets including small mammals, birds,
           reptiles, invertebrate, amphibians and fish.
         </Text>
 
-        <Button
+        <TouchableOpacity
+          style={LoginStyle.LogInButton}
           title="Location"
           onPress={() => navigation.navigate("VetsMap")}
-        />
+        >
+          <Text style={LoginStyle.buttonText}>Location</Text>
+        </TouchableOpacity>
       </List.Accordion>
 
       <List.Accordion
+        style={LoginStyle.buttonText}
         title="Pet Shops"
         left={(props) => <List.Icon {...props} icon="folder" />}
       >
-        <Text
-          style="text-indent: 50px;
-  text-align: justify;
-  letter-spacing: 3px;"
-        >
+        <Text style={LoginStyle.LogInButton}>
           Find an exotic pet shop near you that. There are shopss that sellitems
           for a large variety of exotics pets including small mammals, birds,
           reptiles, invertebrate, amphibians and fish.
         </Text>
 
-        <Button
+        <TouchableOpacity
+          style={LoginStyle.LogInButton}
           title="Location"
           onPress={() => navigation.navigate("ShopsMap")}
-        />
+        >
+          <Text style={LoginStyle.buttonText}>Location</Text>
+        </TouchableOpacity>
       </List.Accordion>
     </List.Section>
   );
